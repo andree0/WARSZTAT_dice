8dfdb00e162f159d60d7a2ca0a1a4db71c872b93
andrzej <jonek.andree@gmail.com>
2020-11-15 18:42:21 +0100

    dice_2

--- dice_2.py
+++ dice_2.py
@@ -1,30 +1,33 @@
 import re
 from random import randint as r
 
-POSSIBLE_DICES = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
+POSSIBLE_DICES = [3, 4, 6, 8, 10, 12, 20, 100]
 CODE = r'^(\d*)D(\d+)([+|-]\d+)*$'
 
 
 def roll_the_dice(dice_code):
     check_code = re.search(CODE, dice_code)
     if check_code:
-        try:
-            multiply = int(check_code.group(1))
-        except ValueError:
-            multiply = 1
-
         dice = int(check_code.group(2))
-        try:
-            modifier = int(check_code.group(3))
-        except (ValueError,TypeError):
-            modifier = 0
+        if dice in POSSIBLE_DICES:
+            try:
+                multiply = int(check_code.group(1))
+            except ValueError:
+                multiply = 1
 
-        result = 0
-        for i in range(multiply):
-            result += r(1, dice)
+            try:
+                modifier = int(check_code.group(3))
+            except (ValueError, TypeError):
+                modifier = 0
 
-        result += modifier
-        return result
+            result = 0
+            for i in range(multiply):
+                result += r(1, dice)
+
+            result += modifier
+            return result
+        else:
+            return "This dice does not exist"
     else:
         return "Wrong input"
 
@@ -37,3 +40,4 @@ if __name__ == '__main__':
     print(roll_the_dice("DD34"))
     print(roll_the_dice("4-3D6"))
     print(roll_the_dice("throw 2D6+15"))
+    print(roll_the_dice("2D5"))
